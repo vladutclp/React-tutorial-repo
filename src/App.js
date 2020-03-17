0a75e5d79271c67a86d54bcdb1f0725682478948
Claps <vladut.clapou@gmail.com>
2020-03-17 20:20:39 +0200

    Some fun with ternary conditions and show/hide button

--- src/App.js
+++ src/App.js
@@ -10,18 +10,19 @@ class App extends Component{
 
    state = {
       persons: [
-        { name: "Claps", age:25 },
-        { name: "Hydra", age:22 },
-        { name: "Kalos", age:21 }
+        { name: "Claps", age:25, id: 1, hidden: false },
+        { name: "Hydra", age:22, id: 2, hidden: false },
+        { name: "Kalos", age:21, id: 3, hidden: false }
       ],
-      otherState: "some other state"
+      otherState: "some other state",
+      hiddenDiv: false
     }
 
     switchNameHandler = (newName) =>{
       this.setState({
         persons:[
-          { name: newName, age:111 },
-          { name: "Hydronex",      age:11 },
+          { name: newName,         age:111 },
+          { name: "Hydronex",      age:11  },
           { name: "Asdfgh",        age:110 }
         ]
       })
@@ -39,29 +40,139 @@ class App extends Component{
       console.log(event);
     }
 
+    togglePersonsHandler = (event) => {
+
+        const btnId = event.target.id;
+        switch(btnId){
+          case "0":
+          console.log(btnId);
+            this.setState ({
+              persons: [
+                { name: "Claps", age:25, id: 1, hidden: !this.state.persons[0].hidden },
+                { name: "Hydra", age:22, id: 2, hidden: this.state.persons[1].hidden },
+                { name: "Kalos", age:21, id: 3, hidden: this.state.persons[2].hidden }
+                ]
+            })
+            break;
+          case "1":
+          console.log(btnId);
+            this.setState ({
+              persons: [
+                { name: "Claps", age:25, id: 1, hidden: this.state.persons[0].hidden },
+                { name: "Hydra", age:22, id: 2, hidden: !this.state.persons[1].hidden },
+                { name: "Kalos", age:21, id: 3, hidden: this.state.persons[2].hidden }
+                ]
+            })
+            break;
+          case "2":
+            console.log(btnId);
+            this.setState ({
+              persons: [
+                { name: "Claps", age:25, id: 1, hidden: this.state.persons[0].hidden },
+                { name: "Hydra", age:22, id: 2, hidden: this.state.persons[1].hidden },
+                { name: "Kalos", age:21, id: 3, hidden: !this.state.persons[2].hidden }
+                ]
+            })
+            break;
+          default:
+            break;
+        }
+        
+          /*
+          const hiddenDiv = this.state.hiddenDiv
+          this.setState({
+            hiddenDiv: !hiddenDiv
+          })
+          */
+      console.log(event.target.id);
+    }
+
   render(){
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: '#4CAF50',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      color: 'white',
+      outline: 'none'
     };
-
+/*
     return(
       <div className="App">
         <h1>Hi I m a React App </h1>
         <button style={style}
-        onClick={() =>this.switchNameHandler("Claponex")}>Switch name</button>
-        <Person name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} 
-                age={this.state.persons[1].age} 
-                click={this.switchNameHandler.bind(this, 'Hydoxiapalytaxe')}
-                changed={this.nameChangedHandler}>Hobbies: Photography</Person>
-        <Person name={this.state.persons[2].name} 
-                age={this.state.persons[2].age} />
+        onClick={this.togglePersonsHandler}>Switch name</button>
+        {
+          this.state.hiddenDiv === true && this.state.person[0].id ===1 ? 
+          <div>
+            <Person name={this.state.persons[0].name} 
+                    age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} 
+                    age={this.state.persons[1].age} 
+                    click={this.switchNameHandler.bind(this, 'Hydoxiapalytaxe')}
+                    changed={this.nameChangedHandler}>Hobbies: Photography</Person>
+            <Person name={this.state.persons[2].name} 
+                    age={this.state.persons[2].age} /> 
+          </div> : null
+        }
+      </div>
+    )
+*/  
+
+    
+    return(
+      <div className="App">
+        <h1>Hi I m a React App </h1>
+
+        <div className="personWrapper"> 
+          <button style={style}
+                  onClick={this.togglePersonsHandler} id="0">{
+            this.state.persons[0].hidden === true ? "Hide me" : "Show me" }
+          </button>
+          
+          {
+            this.state.persons[0].hidden === true && this.state.persons[0].id === 1 ? 
+            <div>
+              <Person name={this.state.persons[0].name} 
+                      age={this.state.persons[0].age} />
+            </div> : null 
+          }
+        </div>
+
+        <div className="personWrapper">
+          <button style={style}
+                  onClick={this.togglePersonsHandler} id="1">{
+            this.state.persons[1].hidden === true ? "Hide me" : "Show me" }
+          </button>
+          
+          {
+            this.state.persons[1].hidden === true && this.state.persons[1].id === 2 ? 
+            <div>
+              <Person name={this.state.persons[1].name} 
+                      age={this.state.persons[1].age} />
+            </div> : null 
+          }
+        </div>
+
+        <div className="personWrapper">
+          <button style={style}
+                  onClick={this.togglePersonsHandler} id="2"> {
+            this.state.persons[2].hidden === true ? "Hide me" : "Show me" }
+          </button>
+          
+          {
+            this.state.persons[2].hidden === true && this.state.persons[2].id === 3 ? 
+            <div>
+              <Person name={this.state.persons[2].name} 
+                      age={this.state.persons[2].age} />
+            </div> : null 
+          }
+        </div>
+
+         
+        
       </div>
     )
   }
